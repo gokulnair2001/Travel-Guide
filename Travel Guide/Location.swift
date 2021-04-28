223af4fe96fa64bdfadfbe3cfe81271b639d6dcf
“gokulnair2001” <gokulnair.2001@gmail.com>
2021-04-29 00:45:49 +0530

    dummy data added

---
+++ Travel Guide/Location.swift
@@ -0,0 +1,22 @@
+//
+//  Location.swift
+//  Travel Guide
+//
+//  Created by Gokul Nair on 28/04/21.
+//
+
+import Foundation
+
+struct Location {
+    let id: Int
+    let name: String
+    let country: String
+    let description: String
+    let more: String
+    let latitude: Double
+    let longitude: Double
+    let heroPicture: String
+    let advisory: String
+    
+    static let example = Location(id: 1, name: "Taj Mahal", country: "India", description: "Made by Shahjahan", more: "For his wife", latitude: 89.00, longitude: 88.09, heroPicture: "smokies", advisory: "beware of monkeys!")
+}
---
+++ Travel Guide/Places.swift
@@ -0,0 +1,12 @@
+//
+//  Places.swift
+//  Travel Guide
+//
+//  Created by Gokul Nair on 29/04/21.
+//
+
+import Foundation
+
+class Locations: ObservableObject {
+    
+}
